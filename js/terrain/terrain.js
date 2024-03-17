df1bf833f39f8ca9dbe277ad93039902e1b6624a
Petally <petalimium@gmail.com>
2024-03-18 00:37:17 +0200

    bugfix: revamp terrain rendering to work on any canvas size

--- js/terrain/terrain.js
+++ js/terrain/terrain.js
@@ -24,21 +24,24 @@ class Terrain {
     }
   }
   /* Draws visible landscape on screen */
+  /* Works with canvases of any size */
   drawTerrain(canvas, imageData, camera) {
-    /* render green for terrain, blue for sky */
     const width = canvas.width;
     const height = canvas.height;
     let data = imageData.data;
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        const cell = this.terrain[(y + Math.floor(camera.position.y)) * this.width + (x + Math.floor(camera.position.x))];
-        const color = (cell === 0) ? [50, 50, 255] : [50, 200, 50];
-        const index = y * (this.width * 2) + x * 4;
-        data[index + 0] = color[0];
-        data[index + 1] = color[1];
-        data[index + 2] = color[2];
-        data[index + 3] = 255;
-      }
+    for (let i = 0; i < width * height; i++) {
+      /* Don't ask me how I managed to make this work correctly. */
+      const y = Math.floor(i / height) + Math.floor(camera.position.y);
+      const x = (i % width) + Math.floor(camera.position.x);
+      const cell = this.terrain[y * this.width + x];
+
+      /* render green for terrain, blue for sky */
+      const color = (cell === 0) ? [50, 50, 255] : [50, 200, 50];
+      const index = i * 4;
+      data[index + 0] = color[0];
+      data[index + 1] = color[1];
+      data[index + 2] = color[2];
+      data[index + 3] = 255;
     }
   }
 }
