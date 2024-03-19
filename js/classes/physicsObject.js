cf1cd541a651ab18ec95908ca16bd43e9fd6cb29
Petally <petalimium@gmail.com>
2024-03-19 19:51:16 +0200

    fix: physics engine reflection angle bug

--- js/classes/physicsObject.js
+++ js/classes/physicsObject.js
@@ -26,27 +26,31 @@ class PhysicsObject {
 
   /* Expects terrain object */
   update(terrain) {
+    /* Test variable, remove later when actual timestep is implemented */
+    const dt = 0.008;
     /* Apply gravity */
-    this.acceleration.y += 0.1;
+    this.acceleration.y += 2;
 
     /* Update velocity */
-    this.velocity = this.velocity.add(this.acceleration);
+    this.velocity = this.velocity.add(this.acceleration.multiply(dt));
 
     /* Update position */
-    const potentialPosition = this.position.add(this.velocity);
+    const potentialPosition = this.position.add(this.velocity.multiply(dt));
 
     /* Reset acceleration */
     this.acceleration = new Vector(0, 0);
     this.stable = false;
 
     /* Collision check with terrain */
-    const angle = Math.atan(this.velocity.x, this.velocity.y);
+    const angle = Math.atan2(this.velocity.y, this.velocity.x);
     let response = new Vector(0, 0);
     let collision = false;
 
     for (let r = angle - Math.PI / 2; r < angle + Math.PI / 2; r += Math.PI / 8) {
-      let testPosX = (this.radius) * Math.cos(r) + potentialPosition.x;
-      let testPosY = (this.radius) * Math.sin(r) + potentialPosition.y;
+      /* Calculate test point on circumfrence of circle */
+      let testPosX = this.radius * Math.cos(r) + potentialPosition.x;
+      let testPosY = this.radius * Math.sin(r) + potentialPosition.y;
+
       testPosX = clamp(testPosX, 0, terrain.width - 1);
       testPosY = clamp(testPosY, 0, terrain.height - 1);
 
@@ -55,21 +59,21 @@ class PhysicsObject {
       if (terrain.terrain[terrainIndex] !== 0) {
         /* Accumulate collision points to give an escape response */
         /* Effectively, normal to the areas of contact */
-        response = response.add(potentialPosition.subtract(new Vector(testPosX, testPosY)));
+        response.x += potentialPosition.x - testPosX;
+        response.y += potentialPosition.y - testPosY;
         collision = true;
       }
     }
 
-    let magVelocity = 0;
+    let magVelocity = this.velocity.magnitude();
+    let magResponse = response.magnitude();
     /* Find angle of collision */
     if (collision) {
       /* Force the object to be stable, this stops the object penetrating the terrain */
       this.stable = true;
-      magVelocity = this.velocity.magnitude();
-      const magResponse = response.magnitude();
 
       /* Calculate reflection vector of object's velocity vector, using response vector */
-      const dot = Vector.dot(this.velocity, response.unit());
+      const dot = this.velocity.x * (response.x / magResponse) + this.velocity.y * (response.y / magResponse);
 
       /* Use friction coefficient to dampen response (approximate energy loss) */
       this.velocity.x = this.friction * (-2 * dot * (response.x / magResponse) + this.velocity.x);
@@ -92,6 +96,7 @@ class PhysicsObject {
         }
       }
     } else {
+      /* If no collision, then just move to the potentialPosition */
       this.position = potentialPosition;
     }
 
